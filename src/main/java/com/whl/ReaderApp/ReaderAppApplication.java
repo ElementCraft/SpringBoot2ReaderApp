cb939ee52b449b6bfe4b76a9e775c89720084f65
小王子(基鸿) <976228284@qq.com>
2018-03-31 19:02:30 +0800

    add cors config

--- src/main/java/com/whl/ReaderApp/ReaderAppApplication.java
+++ src/main/java/com/whl/ReaderApp/ReaderAppApplication.java
@@ -2,9 +2,14 @@ package com.whl.ReaderApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableAsync
